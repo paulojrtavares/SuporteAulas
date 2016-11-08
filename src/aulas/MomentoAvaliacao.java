353efda923f35eca0b8afbfbc53820f7d76e02d2
Userpl022pc20 <Userpl022pc20@SLFRPL022PC20.ensino.atec.local>
2016-11-08 11:41:27 +0000

    commit SuporteAulas

---
+++ src/aulas/MomentoAvaliacao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aulas;
+
+/**
+ *
+ * @author Userpl022pc20
+ */
+public class MomentoAvaliacao {
+    private int nota;
+    private int dificuldade;
+    private String data;
+    private Aluno aluno;
+}
---
+++ src/aulas/ProvaOral.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aulas;
+
+/**
+ *
+ * @author Userpl022pc20
+ */
+public class ProvaOral extends MomentoAvaliacao{
+    private int fluencia;
+    int dominioMateria;
+
+    public int getFluencia() {
+        return fluencia;
+    }
+
+    public void setFluencia(int novaFluencia) {
+        this.fluencia = novaFluencia;
+    }
+
+ 
+    }
+    
+
